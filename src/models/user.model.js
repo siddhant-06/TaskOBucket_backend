35720f63498c138d6d7e91d4330a46085c165df3
siddhant-06 <siddhant@quantumbot.co.in>
2026-01-05 12:04:41 +0530

    feat(user): create user model with schema definition

---
+++ src/models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    avatarUrl: { type: String },
+    jobTitle: { type: String },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export default User;
